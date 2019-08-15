e6b2aa06827db04321cc4908af700435846b891a
Hannah <hannah.kriegler@aws-institut.de>
2019-08-15 11:56:56 +0200

    changed room system

--- Assets/_Scripts/Rooms/Rooms.cs
+++ Assets/_Scripts/Rooms/Rooms.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using UnityEngine;
 
-public abstract class Rooms : MonoBehaviour
+public class Rooms : MonoBehaviour
 {
     public enum RoomTypes
     {
@@ -35,18 +35,74 @@ public abstract class Rooms : MonoBehaviour
         this.collectedLoot = collectedLoot;
     }
 
-    public void OnTriggerEnter2D(Collider2D player)
+    private void Start()
     {
-        if (player.CompareTag("Player"))
-        {
-            RoomBehaviour();
-        }
+        
     }
 
-    public virtual void RoomBehaviour()
+    public void OnTriggerEnter2D(Collider2D player)
     {
-        Debug.Log("Missing Behaviour");
+        if (!player.CompareTag("Player")) return;
+
+        switch (roomTypesType)
+        {
+            case RoomTypes.Tutorial:
+                TutorialRoomBehaviour();
+                break;
+            case RoomTypes.Spawn:
+                SpawnRoomBehaviour();
+                break;
+            case RoomTypes.Normal:
+                NormalRoomBehaviour();
+                break;
+            case RoomTypes.Mob:
+                MobRoomBehaviour();
+                break;
+            case RoomTypes.Loot:
+                LootRoomBehaviour();
+                break;
+            case RoomTypes.Boss:
+                BossRoomBehaviour();
+                break;
+
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+        
+        Debug.Log("Enemies alive: " + aliveEnemies.Length);
+        
     }
     
+
+    private void TutorialRoomBehaviour()
+    {
+        Debug.Log("Welcome to the Tutorial Room!");
+    }
+
+    private void SpawnRoomBehaviour()
+    {
+        Debug.Log(("Welcome to the Spawn Romm!"));
+    }
+
+    private void NormalRoomBehaviour()
+    {
+        Debug.Log("Welcome to a Normal Room!");
+    }
+
+    private void MobRoomBehaviour()
+    {
+        Debug.Log("Welcome to a Mob Room!");
+    }
+
+    private void BossRoomBehaviour()
+    {
+        Debug.Log("Welcome to a Boss Room!");
+    }
+
+    private void LootRoomBehaviour()
+    {
+        Debug.Log("Welcome to a Loot Room!");
+    }
+
     
 }
