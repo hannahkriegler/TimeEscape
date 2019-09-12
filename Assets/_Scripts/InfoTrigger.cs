0543f088304ecc760681f39a362f31dd7bd34b2c
Philip_Dev <philipchell@hotmail.com>
2019-09-13 01:22:12 +0200

    hotfix

--- Assets/_Scripts/InfoTrigger.cs
+++ Assets/_Scripts/InfoTrigger.cs
@@ -10,11 +10,17 @@ namespace TE
         public string message;
         public float duration = 3.0f;
 
+        bool triggerd;
+
         private void OnTriggerEnter2D(Collider2D collision)
         {
+            if (triggerd)
+                return;
+
             if (collision.gameObject.CompareTag("Player"))
             {
                 Game.instance.ShowInfo(message, duration);
+                triggerd = true;
             }
         }
     }
