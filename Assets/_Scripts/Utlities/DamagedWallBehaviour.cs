adb6c96d139dea85720fabff26f64772ad4a1f1f
Hannah <hannah.kriegler@aws-institut.de>
2019-10-08 18:34:39 +0200

    added damaged walls, movement still buggy

---
+++ Assets/_Scripts/Utlities/DamagedWallBehaviour.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TE;
+using UnityEngine;
+
+public class DamagedWallBehaviour : MonoBehaviour
+{
+    public bool shake;
+    public float timeToShake = 2f;
+    private float currentTime;
+    private void OnTriggerEnter(Collider other)
+    {
+        if(!other.CompareTag("FireBall")) return;
+    }
+
+    private void DestroyWall()
+    {
+        
+    }
+
+    private void Update()
+    {
+        if (shake)
+        {
+            StartCoroutine(MoveWallPart());
+            shake = false;
+        }
+    }
+
+
+    private IEnumerator MoveWallPart()
+    {
+        currentTime = timeToShake;
+        while (currentTime > 0)
+        {
+            foreach (Transform wall in transform)
+            {
+                Debug.Log(wall.name);
+                wall.transform.localRotation = new Quaternion(wall.transform.localRotation.x,
+                    wall.transform.localRotation.y, Mathf.Sin(Time.time * 10f) * .07f, 1);
+            }
+
+            currentTime -= Time.deltaTime;
+        }
+
+        yield return null;
+    }
+    
+    
+}
